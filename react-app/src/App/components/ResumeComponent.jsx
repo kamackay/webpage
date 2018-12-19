d0d5dc718e074c7a419a56f9a3a14318aedbba6d
Keith MacKay <keith.mackay3@gmail.com>
2018-12-19 11:30:27 -0500

    Added Tech logos to skills list, also added color to the bar

--- react-app/src/App/components/ResumeComponent.jsx
+++ react-app/src/App/components/ResumeComponent.jsx
@@ -1,6 +1,49 @@
 import React, { Component } from "react";
 
+const percentColors = [
+	{ pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
+	{ pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+	{ pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } }
+];
+
 class ResumeComponent extends Component {
+	getSkillImage(skill, size) {
+		size = size || "25px";
+		if (skill.img) {
+			return (
+				<img style={{ height: size, width: size }} src={skill.img} />
+			);
+		}
+		return null;
+	}
+
+	getColorForPercentage(pct) {
+		for (var i = 1; i < percentColors.length - 1; i++) {
+			if (pct < percentColors[i].pct) {
+				break;
+			}
+		}
+		var lower = percentColors[i - 1];
+		var upper = percentColors[i];
+		var range = upper.pct - lower.pct;
+		var rangePct = (pct - lower.pct) / range;
+		var pctLower = 1 - rangePct;
+		var pctUpper = rangePct;
+		var color = {
+			b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
+			g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+			r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper)
+		};
+		const s = "rgba(" + [color.r, color.g, color.b].join(",") + ", .75)";
+		// tslint:disable-next-line:no-console
+		console.log(s);
+		return s;
+		// or output as hex if preferred
+	}
+
+	percentStringToNumber(str) {
+		return parseInt(str, 10) / 100;
+	}
 
 	render() {
 		let skillsData;
@@ -10,7 +53,7 @@ class ResumeComponent extends Component {
 
 		if (this.props.data) {
 			skillMessage = this.props.data.skillmessage;
-			educationData = this.props.data.education.map((education) => {
+			educationData = this.props.data.education.map(education => {
 				return (
 					<div key={education.school}>
 						<h3>{education.school}</h3>
@@ -22,7 +65,7 @@ class ResumeComponent extends Component {
 					</div>
 				);
 			});
-			workData = this.props.data.work.map((work) => {
+			workData = this.props.data.work.map(work => {
 				return (
 					<div key={work.company}>
 						<h3>{work.company}</h3>
@@ -36,15 +79,23 @@ class ResumeComponent extends Component {
 				);
 			});
 
-			skillsData = this.props.data.skills.map(skills => {
-				const className = `bar-expand ${skills.name.toLowerCase()}`;
+			skillsData = this.props.data.skills.map(skill => {
+				const className = `bar-expand ${skill.name.toLowerCase()}`;
 				return (
-					<li key={skills.name}>
+					<li key={skill.name}>
 						<span
-							style={{ width: skills.level }}
+							style={{
+								background: this.getColorForPercentage(
+									this.percentStringToNumber(skill.level)
+								),
+								display: "inline",
+								width: skill.level
+							}}
 							className={className}
 						/>
-						<em>{skills.name}</em>
+						<em>
+							{this.getSkillImage(skill)}&nbsp;{skill.name}
+						</em>
 					</li>
 				);
 			});

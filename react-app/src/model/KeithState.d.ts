a3fc35f1d24c2c1e9a67f0c760cd048b7741b34f
Keith MacKay <keith.mackay3@gmail.com>
2018-12-20 14:47:09 -0500

    Change State to not be in a d file

--- react-app/src/model/KeithState.d.ts
+++
@@ -1,6 +0,0 @@
-declare class KeithState {
-	title?: string;
-	faviconUrl?: string;
-}
-
-export default KeithState;
---
+++ react-app/src/model/KeithState.ts
@@ -0,0 +1,7 @@
+class KeithState {
+	public title?: string;
+	public faviconUrl?: string;
+	public loading: boolean = true;
+}
+
+export default KeithState;

0befb9252ae751bafe4db0d76b10cd4c705559ab
Keith <keith@keithmackay.com>
2020-08-22 13:37:28 -0400

    Add Sidecar to track Logs

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build . -t $IMAGE && \
     # docker-squash "$IMAGE" --tag "$IMAGE" && \
     docker push $IMAGE && \
     kubectl --context do-nyc3-keithmackay-cluster -n webpage \
-    set image ds/webpage webpage=$IMAGE
+    set image ds/webpage server=$IMAGE
